0e9ce8747e1658e3a0e3e666456cf9ef2eb156ae
Reagan Zierke <reaganzierke@gmail.com>
2025-03-04 20:15:46 -0600

    modified chirp model and created reply model

--- chirps/models.py
+++ chirps/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-"from ___ import user"
+from django.contrib.auth.models import User
+
 
 class Chirp(models.Model):
-    user = user
-    content = models.CharField(max_length=250)
-    reply = models.CharField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
--- replies/models.py
+++ replies/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from chirps.models import Chirp
 # Create your models here.
 
-class Replies(Chirp):
-    
+class Reply(Chirp):
+    reply_to = models.ForeignKey(Chirp, on_delete=models.CASCADE, related_name="replies")
+    reply_content = models.CharField(max_length=255)
